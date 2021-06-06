f8b38af7538673a80252b223b24b6b7509c37be9
nzemnytskyi <nikita.zemnytskyi@cengage.com>
2021-06-06 14:15:51 +0300

    Prepare content

---
+++ src/main/java/com/zemnitskiy/Calculator.java
@@ -0,0 +1,14 @@
+package com.zemnitskiy;
+
+public class Calculator {
+
+    public int plus(int a, int b) {
+        System.out.println("Plus operation");
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        System.out.println("Minus operation");
+        return a - b;
+    }
+}
---
+++ src/test/java/com/zemnitskiy/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.zemnitskiy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void plus() {
+        int expect = 4;
+        int result = calculator.plus(2,2);
+        assertEquals(expect, result);
+        System.out.println("Test 1 passed");
+    }
+
+    @Test
+    void minus() {
+        int expect = 0;
+        int result = calculator.minus(2,2);
+        assertEquals(expect, result);
+        System.out.println("Test 2 passed");
+    }
+}
